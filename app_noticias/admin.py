9825e2060e68bbfad6e21ae4cd2ce6974fb7eecd
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-10-27 16:02:30 -0300

    Feat: inclui as funcionalidades de logs e de categorias

--- app_noticias/admin.py
+++ app_noticias/admin.py
@@ -4,7 +4,7 @@ ADMIN: Registrar modelos da aplicação na área administrativa.
 
 from django.contrib import admin
 from .models import (
-
+    UserAction,
     Categoria,
     Noticia
 )
@@ -19,7 +19,6 @@ class CategoriaAdmin(admin.ModelAdmin):
     ]
 
     search_fields = ['nome']
-
 
 
 class NoticiaAdmin(admin.ModelAdmin):
@@ -46,6 +45,19 @@ class NoticiaAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
+class UserActionAdmin(admin.ModelAdmin):
+    '''
+    UserActionAdmin
+    '''
+    list_filter = ['user', 'object_name']
+    search_fields = ['user__username']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.autor = request.user
+        super().save_model(request, obj, form, change)
+
+
 admin.site.register(Noticia, NoticiaAdmin)
 admin.site.register(Categoria, CategoriaAdmin)
-
+admin.site.register(UserAction, UserActionAdmin)
